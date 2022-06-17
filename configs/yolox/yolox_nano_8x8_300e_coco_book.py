3bb6fdb2fd79a407be104dd790a53119075b9433
Albin <albin.zhu.me@gmail.com>
2022-06-17 10:38:57 +0800

    input size 1024 config

--- configs/yolox/yolox_nano_8x8_300e_coco_book.py
+++ configs/yolox/yolox_nano_8x8_300e_coco_book.py
@@ -2,7 +2,7 @@
 Author: bin.zhu
 Date: 2022-06-14 11:29:44
 LastEditors: bin.zhu
-LastEditTime: 2022-06-16 13:26:02
+LastEditTime: 2022-06-17 10:38:03
 Description: file content
 '''
 
@@ -19,6 +19,43 @@ model = dict(
         use_depthwise=True),
     bbox_head=dict(
         num_classes=9, in_channels=64, feat_channels=64, use_depthwise=True))
+img_scale = (1024, 1024)  # height, width
+
+train_pipeline = [
+    dict(type='Mosaic', img_scale=img_scale, pad_val=114.0),
+    dict(
+        type='RandomAffine',
+        scaling_ratio_range=(0.5, 1.5),
+        border=(-img_scale[0] // 2, -img_scale[1] // 2)),
+    dict(type='YOLOXHSVRandomAug'),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Resize', img_scale=img_scale, keep_ratio=True),
+    dict(
+        type='Pad',
+        pad_to_square=True,
+        pad_val=dict(img=(114.0, 114.0, 114.0))),
+    dict(type='FilterAnnotations', min_gt_bbox_wh=(1, 1), keep_empty=False),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(
+                type='Pad',
+                pad_to_square=True,
+                pad_val=dict(img=(114.0, 114.0, 114.0))),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img'])
+        ])
+]
 
 train_dataset = dict(
     type='MultiImageMixDataset',
@@ -32,24 +69,7 @@ train_dataset = dict(
         ],
         filter_empty_gt=False,
     ),
-    pipeline=[
-        dict(type='Mosaic', img_scale=(1024, 1024), pad_val=114.0),
-        dict(
-            type='RandomAffine',
-            scaling_ratio_range=(0.5, 1.5),
-            border=(-320, -320)),
-        dict(type='YOLOXHSVRandomAug'),
-        dict(type='RandomFlip', flip_ratio=0.5),
-        dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
-        dict(
-            type='Pad',
-            pad_to_square=True,
-            pad_val=dict(img=(114.0, 114.0, 114.0))),
-        dict(
-            type='FilterAnnotations', min_gt_bbox_wh=(1, 1), keep_empty=False),
-        dict(type='DefaultFormatBundle'),
-        dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
-    ]),
+    pipeline=train_pipeline),
 data = dict(
     samples_per_gpu=20,
     workers_per_gpu=4,
@@ -58,43 +78,10 @@ data = dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/val.json',
         img_prefix=data_root + 'val/',
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(
-                type='MultiScaleFlipAug',
-                img_scale=(1024, 1024),
-                flip=False,
-                transforms=[
-                    dict(type='Resize', keep_ratio=True),
-                    dict(type='RandomFlip'),
-                    dict(
-                        type='Pad',
-                        pad_to_square=True,
-                        pad_val=dict(img=(114.0, 114.0, 114.0))),
-                    dict(type='DefaultFormatBundle'),
-                    dict(type='Collect', keys=['img'])
-                ])
-        ]
-        ),
+        pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/val.json',
         img_prefix=data_root + 'val/',
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(
-                type='MultiScaleFlipAug',
-                img_scale=(1024, 1024),
-                flip=False,
-                transforms=[
-                    dict(type='Resize', keep_ratio=True),
-                    dict(type='RandomFlip'),
-                    dict(
-                        type='Pad',
-                        pad_to_square=True,
-                        pad_val=dict(img=(114.0, 114.0, 114.0))),
-                    dict(type='DefaultFormatBundle'),
-                    dict(type='Collect', keys=['img'])
-                ])
-        ]))
+        pipeline=test_pipeline))
 load_from="/albin/yolox_tiny_8x8_300e_coco_20211124_171234-b4047906.pth"
